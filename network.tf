94779fd89640ab28ba7e2853384c7e88d5a7faae
Leif Eriksen <leif.eriksen.au@gmail.com>
2025-09-18 12:13:11 +1000

    restore after bad cleanup

--- network.tf
+++ network.tf
@@ -90,3 +90,59 @@ resource "azurerm_public_ip" "ip01" {
   allocation_method   = "Static"
 }
 
+# resource "azurerm_private_endpoint" "psql-pe01-fs" {
+#   location            = azurerm_resource_group.rg.location
+#   name                = "psql-pe01-fs"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   subnet_id           = azurerm_subnet.pe01.id
+#
+#   private_service_connection {
+#     name                           = "psql-pe01-fs-psc"
+#     private_connection_resource_id = data.azurerm_postgresql_flexible_server.fs.id
+#     subresource_names              = ["postgresqlServer"]
+#     is_manual_connection           = false
+#   }
+#
+#   private_dns_zone_group {
+#     name                 = "psql-pe01-fs-dns-zone-group"
+#     private_dns_zone_ids = [azurerm_private_dns_zone.psql.id]
+#   }
+# }
+#
+# resource "azurerm_private_endpoint" "psql-pe01-fs-replica" {
+#   location            = azurerm_resource_group.rg.location
+#   name                = "psql-pe01-fs-replica"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   subnet_id           = azurerm_subnet.pe01.id
+#
+#   private_service_connection {
+#     name                           = "psql-pe01-fs-replica-psc"
+#     private_connection_resource_id = data.azurerm_postgresql_flexible_server.fs_replica.id
+#     subresource_names              = ["postgresqlServer"]
+#     is_manual_connection           = false
+#   }
+#
+#   private_dns_zone_group {
+#     name                 = "psql-pe01-fs-dns-zone-group"
+#     private_dns_zone_ids = [azurerm_private_dns_zone.psql.id]
+#   }
+# }
+
+resource "azurerm_private_endpoint" "psql-pe01-ec" {
+  location            = azurerm_resource_group.rg.location
+  name                = "psql-pe010-ec"
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.pe01.id
+
+  private_service_connection {
+    name                           = "psql-pe01-ec-psc"
+    private_connection_resource_id = data.azurerm_postgresql_flexible_server.ec.id
+    subresource_names              = ["postgresqlServer"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "psql-pe01-ec-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.psql.id]
+  }
+}
